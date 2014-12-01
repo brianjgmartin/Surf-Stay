49b812cfd0b1114350a39e20c98a3b41c4ff91b3
Brian Martin <brianjgmartin@gmail.com>
2014-12-01 13:20:59 +0000

    add jquery file

--- bootstrap-3.2.0-dist/my1.php
+++ bootstrap-3.2.0-dist/my1.php
@@ -12,12 +12,11 @@
     <link rel="stylesheet" type="text/css" href="../bootstrap-3.2.0-dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../bootstrap-3.2.0-dist/css/weather-icons.css">
     <link rel="stylesheet" type="text/css" href="../bootstrap-3.2.0-dist/css/myStyle.css">
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+   <script type="text/javascript" src="../bootstrap-3.2.0-dist/silviomoreto-bootstrap-select-83d5a1b/dist/js/bootstrap-select.js"></script>
+   <script type="text/javascript" src="../bootstrap-3.2.0-dist/js/jquery-2.1.1.min.js"></script>
+
  
-  <?php include 'm.php';?>
+  <?php include './m.php';?>
  
     <script type="text/javascript">
         $(document).ready(function(){ 
@@ -327,8 +326,8 @@
          
      <!-- // <script src="http://code.highcharts.com/highcharts.js"></script> -->
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+       <!--  // <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> -->
         <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="js/bootstrap.min.js"></script>
+        <script src="../bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
   </body>
 </html>
